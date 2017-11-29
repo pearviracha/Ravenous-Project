fdd5bd184948576eb27bfc869ef795c30523e0b7
Pear Poolvaraluk <32940272+pearviracha@users.noreply.github.com>
2017-11-29 21:21:06 +0000

    Update App.js

--- App.js
+++ App.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import BusinessList from '../BusinessList/BusinessList';
+import SearchBar from './Searchbar/SearchBar';
 
 class App extends Component {
   render() {
@@ -34,3 +36,32 @@ class BusinessList extends React.Component {
  }
 
  module.exports=BusinessList;
+
+ class SearchBar extends React.Component {
+   renderSortByOptions() {
+     return
+     Object.keys(sortByOptions).map(sortByOptions => {
+       let sortByOptionValue =
+       sortByOptions[sortByOption];
+       return <li key={sortByOptionValue}> {sortByOption} </li>;
+     });
+   }
+   render() {
+     return <div className="SearchBar">
+  <div className="SearchBar-sort-options">
+    <ul>
+      <{renderSortByOptions}/>
+    </ul>
+  </div>
+  <div className="SearchBar-fields">
+    <input placeholder="Search Businesses" />
+    <input placeholder="Where?" />
+  </div>
+  <div className="SearchBar-submit">
+    <a>Lets Go</a>
+    </div>
+</div>
+   }
+ };
+
+module.exports=SearchBar;
