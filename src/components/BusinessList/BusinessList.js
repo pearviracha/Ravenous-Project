49bcf50160061cbd771e714838f8ebd49cff8c0c
Pear Poolvaraluk <32940272+pearviracha@users.noreply.github.com>
2017-12-13 14:18:12 +0000

    Update BusinessList.js

--- src/components/BusinessList/BusinessList.js
+++ src/components/BusinessList/BusinessList.js
@@ -4,14 +4,29 @@ import './BusinessList.css';
 import Business from '../Business/Business';
 
 class BusinessList extends React.Component {
-   render() {
-     return (
-  <div className="BusinessList">
-    {
-      this.props.businesses.map(business => {
-        return <Business business={business} key={business.id} />
-      })
-    }
+componentDidMount(){
+console.log(this.props);
+}
+render() {
+return (
+<div className="BusinessList">
+  <ul>{this.props.business.map(place => {
+  return <li>{place.address}</li>
+  <li>{place.city}</li>
+  <li>{place.state}</li>
+  <li>{place.zipCode}</li>
+  })}
+  </ul>
+</div>
+);
+        <p>{`${this.props.business.state} ${this.props.business.zipCode}`}</p>
+      </div>
+      <div className="Business-reviews">
+        <h3>{this.props.business.category}</h3>
+        <h3 className="rating">
+        {this.props.business.rating}</h3>
+      <p>{this.props.business.reviewCount}</p>
+      </div>
      </div>
   );
 }
